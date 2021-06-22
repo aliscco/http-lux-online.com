644fb97075c644ce2c018f1727186556c377b444
Ali mazloum <86027817+aliscco@users.noreply.github.com>
2021-06-22 10:58:31 +0300

    Initialize fix/docusaurus_v2

---
+++ src/test/fixtures/fake-event-store.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { EventEmitter } = require('events');
+
+class EventStore extends EventEmitter {
+    constructor() {
+        super();
+        this.setMaxListeners(0);
+        this.events = [];
+    }
+
+    store(event) {
+        this.events.push(event);
+        this.emit(event.type, event);
+        return Promise.resolve();
+    }
+
+    batchStore(events) {
+        events.forEach(event => {
+            this.events.push(event);
+            this.emit(event.type, event);
+        });
+        return Promise.resolve();
+    }
+
+    getEvents() {
+        return Promise.resolve(this.events);
+    }
+}
+
+module.exports = EventStore;
