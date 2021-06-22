9ac0fa4be9b6d64a3eb73ad5848a526d9fb6004e
Ali mazloum <86027817+aliscco@users.noreply.github.com>
2021-06-22 10:57:56 +0300

    Initialize feat-690-toggle-filtering

---
+++ test/fixtures/store.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const ClientMetricsStore = require('./fake-metrics-store');
+const clientInstanceStore = require('./fake-client-instance-store');
+const clientApplicationsStore = require('./fake-client-applications-store');
+const featureToggleStore = require('./fake-feature-toggle-store');
+const tagStore = require('./fake-tag-store');
+const eventStore = require('./fake-event-store');
+const strategyStore = require('./fake-strategies-store');
+const contextFieldStore = require('./fake-context-store');
+const settingStore = require('./fake-setting-store');
+
+module.exports = {
+    createStores: () => {
+        const db = {
+            select: () => ({
+                from: () => Promise.resolve(),
+            }),
+        };
+
+        return {
+            db,
+            clientApplicationsStore: clientApplicationsStore(),
+            clientMetricsStore: new ClientMetricsStore(),
+            clientInstanceStore: clientInstanceStore(),
+            featureToggleStore: featureToggleStore(),
+            tagStore: tagStore(),
+            eventStore: eventStore(),
+            strategyStore: strategyStore(),
+            contextFieldStore: contextFieldStore(),
+            settingStore: settingStore(),
+        };
+    },
+};
